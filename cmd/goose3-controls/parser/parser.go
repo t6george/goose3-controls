51d996d5650a8b9d5f5e109407a2feed02932c37
ThomasGeorge <t6george@edu.uwaterloo.ca>
2018-03-24 16:24:04 -0400

    parser and helper modularized

--- cmd/goose3-controls/parser/parser.go
+++ cmd/goose3-controls/parser/parser.go
@@ -1,2 +1,35 @@
 package parser
 
+import (
+	"encoding/json"
+	"fmt"
+	. "goose3-controls/cmd/goose3-controls/helper"
+	"io/ioutil"
+	"os"
+)
+
+func ToJson(c interface{}) string {
+	bytes, err := json.Marshal(c)
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	return string(bytes)
+}
+
+func ToString(c Command) string {
+	return ToJson(c)
+}
+
+func GetCommands() []Command {
+	raw, err := ioutil.ReadFile("./test.json")
+	if err != nil {
+		fmt.Println(err.Error())
+		os.Exit(1)
+	}
+
+	var c []Command
+	json.Unmarshal(raw, &c)
+	return c
+}
